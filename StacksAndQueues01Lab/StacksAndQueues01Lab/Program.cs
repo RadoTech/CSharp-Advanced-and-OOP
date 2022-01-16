74eb4aa08d7d753c986d0cbe904411f667632d55
Radoslav Radev <rado.radev81@gmail.com>
2022-01-16 22:59:52 +0200

    adding program description.

--- StacksAndQueues01Lab/StacksAndQueues01Lab/Program.cs
+++ StacksAndQueues01Lab/StacksAndQueues01Lab/Program.cs
@@ -9,6 +9,8 @@ namespace StacksAndQueues01Lab
         static void Main(string[] args)
         {
             //1. Reverse Strings
+            //Create a program that: Reads an input string; Reverses it using a Stack<T>;
+            //Prints the result back at the terminal
 
             var myStack = new Stack<char>(Console.ReadLine());
 
