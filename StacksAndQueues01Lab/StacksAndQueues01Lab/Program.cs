1812c8b906cf8ee518d0fd9dc790a73679babaa4
Radoslav Radev <rado.radev81@gmail.com>
2022-01-16 22:54:26 +0200

    Adding 01. Reverse Strings exercise from 01. Stacks and Queues

---
+++ StacksAndQueues01Lab/StacksAndQueues01Lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StacksAndQueues01Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Reverse Strings
+
+            var myStack = new Stack<char>(Console.ReadLine());
+
+            while (myStack.Any())
+            {
+                Console.Write(myStack.Pop());
+            }
+        }
+    }
+}
