e8472e9806294885a4080b8547565e6156e32e07
Radoslav Radev <rado.radev81@gmail.com>
2022-02-07 23:34:39 +0200

    Adding Lab exercise 04 from Sets and Dictionaries Advanced

---
+++ SetsAndDictionariesAdv04Lab/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsAndDictionariesAdv04Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productShop = new Dictionary<string, Dictionary<string, double>>();
+
+            //input:
+            //lidl, juice, 2.30
+
+            string input = Console.ReadLine();
+
+            while (input != "Revision")
+            {
+                string[] inputInfo = input
+                                          .Split(", ");
+
+                string shopName = inputInfo[0];
+                string productName = inputInfo[1];
+                double productPrice = double.Parse(inputInfo[2]);
+
+                if (!productShop.ContainsKey(shopName))
+                {
+                    productShop.Add(shopName, new Dictionary<string, double>());
+                }
+                if (!productShop[shopName].ContainsKey(productName))
+                {
+                    productShop[shopName].Add(productName, 0);
+                }
+
+                productShop[shopName][productName] = productPrice;
+
+                input = Console.ReadLine();
+            }
+
+            //output:
+            //fantastico ->
+            //Product: apple, Price: 1.2
+
+            foreach (var (shopName, products) in productShop.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{shopName}->");
+
+                foreach (var currentProduct in products)
+                {
+                    Console.WriteLine($"Product: {currentProduct.Key}, Price: {currentProduct.Value}");
+                }
+            }
+
+
+        }
+    }
+}
