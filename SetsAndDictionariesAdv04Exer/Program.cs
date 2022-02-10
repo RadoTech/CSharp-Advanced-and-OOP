811b70fb7c95878bcd12133e9cca2cf4d9b5d733
Radoslav Radev <rado.radev81@gmail.com>
2022-02-10 23:46:56 +0200

    Adding 04 Exercise from Sets and Dictionaries Advanced

---
+++ SetsAndDictionariesAdv04Exer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsAndDictionariesAdv04Exer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercises: Sets and Dictionaries Advanced
+            //Problem 4.	Even Times
+
+            //Create a program that prints a number from a collection, which appears an even number of times in it. On the first line, 
+            //you will be given n – the count of integers you will receive. On the next n lines, you will be receiving the numbers. 
+            //It is guaranteed that only one of them appears an even number of times. Your task is to find that number and print it in the end. 
+
+            var inputCount = int.Parse(Console.ReadLine());
+            var inputDictA = new Dictionary<int, int>();
+            var nextNum = Console.ReadLine();
+            var inputList= new List<int>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                inputList.Add(int.Parse(nextNum));
+
+                if (i == inputCount -1)
+                {
+                    break;
+                }
+                nextNum = Console.ReadLine();
+            }
+
+            foreach (var item in inputList)
+            {
+                if (inputDictA.ContainsKey(item))
+                {
+                    inputDictA[item]++;
+                }
+                else
+                {
+                    inputDictA.Add(item, 1);
+                }
+            }
+
+            foreach (var item in inputDictA)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+            
+        }
+    }
+}
