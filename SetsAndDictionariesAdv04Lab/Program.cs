84e4b25bf97de66a30641f16b870c16b26875157
Radoslav Radev <rado.radev81@gmail.com>
2022-02-11 02:39:54 +0200

    New better code for 04. Product Shop Lab from Sets and Dictionaries Advanced

--- SetsAndDictionariesAdv04Lab/Program.cs
+++ SetsAndDictionariesAdv04Lab/Program.cs
@@ -8,47 +8,37 @@ namespace SetsAndDictionariesAdv04Lab
     {
         static void Main(string[] args)
         {
-            var productShop = new Dictionary<string, Dictionary<string, double>>();
+            SortedDictionary<string, Dictionary<string, double>> shops = new SortedDictionary<string, Dictionary<string, double>>();
 
             //input:
             //lidl, juice, 2.30
 
-            string input = Console.ReadLine();
+            string command = Console.ReadLine();
 
-            while (input != "Revision")
+            while (command?.ToUpper() != "REVISION")
             {
-                string[] inputInfo = input
-                                          .Split(", ");
+                string[] tokens = command
+                                        .Split(", ", StringSplitOptions.RemoveEmptyEntries);
 
-                string shopName = inputInfo[0];
-                string productName = inputInfo[1];
-                double productPrice = double.Parse(inputInfo[2]);
-
-                if (!productShop.ContainsKey(shopName))
+                if (shops.ContainsKey(tokens[0]) == false) // == !shop.ContainsKey(tokens[0])
                 {
-                    productShop.Add(shopName, new Dictionary<string, double>());
-                }
-                if (!productShop[shopName].ContainsKey(productName))
-                {
-                    productShop[shopName].Add(productName, 0);
+                    shops[tokens[0]] = new Dictionary<string, double>(); //adding the key and creating the nested Dictionary in case missing
                 }
 
-                productShop[shopName][productName] = productPrice;
+                shops[tokens[0]].Add(tokens[1], double.Parse(tokens[2])); //entering the values in the nested Dict after we already have the key
 
-                input = Console.ReadLine();
+                command = Console.ReadLine();
             }
-
             //output:
             //fantastico ->
             //Product: apple, Price: 1.2
-
-            foreach (var (shopName, products) in productShop.OrderBy(x => x.Key))
+            foreach (var shop in shops)
             {
-                Console.WriteLine($"{shopName}->");
+                Console.WriteLine($"{shop.Key}->");
 
-                foreach (var currentProduct in products)
+                foreach (var product in shop.Value)
                 {
-                    Console.WriteLine($"Product: {currentProduct.Key}, Price: {currentProduct.Value}");
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
                 }
             }
 
