fe68f18b1019f191e6c42bacfa6deb424568f04f
Radoslav Radev <rado.radev81@gmail.com>
2022-02-13 22:38:08 +0200

    Adding 06. Wardrobe from Exercises: Sets and Dictionaries Advanced

---
+++ 07.TheVLogger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07.TheVLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
