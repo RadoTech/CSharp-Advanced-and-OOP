d1a3cfc49c0bce02c9ff76ca2c750572ef98474e
Radoslav Radev <rado.radev81@gmail.com>
2022-02-13 22:38:08 +0200

    Adding 06. Wardrobe from Exercises: Sets and Dictionaries Advanced

---
+++ SetsAndDictionariesAdvancedExercises/06.Wardrobe/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputLen = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            fillDictionary(wardrobe, inputLen);
+            string[] clothesToFind = Console.ReadLine().Split(" ");
+            string colorToFind = clothesToFind[0];
+            string clothToFind = clothesToFind[1];
+            printResult(wardrobe, colorToFind, clothToFind);
+        }
+
+        static void printResult(Dictionary<string, Dictionary<string, int>> wardrobe, string colorToFind, string clothToFind)
+        {
+            foreach (KeyValuePair<string, Dictionary<string, int>> kvp in wardrobe)
+            {
+                Console.WriteLine($"{kvp.Key} clothes:");
+
+                foreach (var clothing in kvp.Value)
+                {
+                    if (colorToFind == kvp.Key && clothToFind == clothing.Key)
+                    {
+                        Console.WriteLine($"* {clothing.Key} - {clothing.Value} (found!)"); 
+                        continue;
+                    }
+                    Console.WriteLine($"* {clothing.Key} - {clothing.Value}");
+                }
+            }
+         }
+
+        static Dictionary<string, Dictionary<string, int>> fillDictionary(Dictionary<string, Dictionary<string, int>> wardrobe, int inputLen)
+        {
+            for (int i = 0; i < inputLen; i++)
+            {
+                string[] input = Console.ReadLine().Split(" -> ");
+                string currentColor = input[0]; //Blue
+                string[] clothes = input[1].Split(","); //dress,jeans,hat
+
+                if (!wardrobe.ContainsKey(currentColor))
+                {
+                    wardrobe.Add(currentColor, new Dictionary<string, int>());
+                }
+
+                for (int cloth = 0; cloth < clothes.Length; cloth++)
+                {
+                    if (!wardrobe[currentColor].ContainsKey(clothes[cloth]))
+                    {
+                        wardrobe[currentColor].Add(clothes[cloth], 0);
+                    }
+
+                    wardrobe[currentColor][clothes[cloth]]++;
+                }
+            }
+
+            return wardrobe;
+        }
+    }
+}
